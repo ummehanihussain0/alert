bad12356f95970ad44d2d146c0f32fa51686a052
ummehanihussain0 <121793260+ummehanihussain0@users.noreply.github.com>
2023-03-12 04:05:47 +0500

    Add files via upload

---
+++ alert/javaa/alert.js
@@ -0,0 +1,8 @@
+alert("HELLO! Good Morning");
+alert("Please Enter a valid password");
+alert("Welcome to JS Land...\n Happy Coding");
+alert("Welcome to JS Land...");
+alert("Happy Coding");
+alert("Hello! I can run JS through my web browser's console");
+var ch = 'hello';
+console.log(ch);
